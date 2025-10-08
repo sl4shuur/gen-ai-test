ef58d5f9ed2f75f109fa601bf45ec15247ef619e
Denys Mykhailov <dmykhailov@kse.org.ua>
2025-10-08 16:52:36 +0300

    feat: add directory creation logic for data directory in config

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,4 +1,3 @@
-from pathlib import Path
 try:
     from .config_helper import find_project_root
 except ImportError:
@@ -7,12 +6,21 @@ except ImportError:
 # Base of the project
 BASE_DIR = find_project_root()
 
+DATA_DIR = BASE_DIR / "data"
+
 # # .env file loading
 # dotenv_path = BASE_DIR / ".env"
 # if not dotenv_path.exists():
 #     raise FileNotFoundError(f"Environment file not found: {dotenv_path}")
 
 # dotenv.load_dotenv(dotenv_path)
+
+# check if the directories exist, if not create them
+for directory in [DATA_DIR]:
+    if directory.exists() and directory.is_file():
+        print(f"Warning: {directory} is a file, expected a directory. Recreating as directory.")
+        directory.unlink()  # Remove the file if a file exists with the same name
+    directory.mkdir(parents=True, exist_ok=True)
 
 
 if __name__ == "__main__":
